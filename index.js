856e26b6e351c784010a094b19005f88658e3c23
Tobiyouth <ljtian516521@qq.com>
2022-08-24 23:30:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    book: {
+        assets: './lib',
+        js: [
+            'viewer.min.js',
+            'main.js'
+        ],
+        css: [
+            'viewer.min.css'
+        ]
+    }
+};
